9bc019658eeb4353dbfcd0a7ccaddcc2f67fd257
Sebastian Linares <slinaresl@gmail.com>
2019-06-11 19:03:09 -0400

    Updates

---
+++ precache-manifest.b644eb09dff2ec6d6c676b9e33a9320f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4014a04133a0fbd9e53ac7cd70854f",
+    "url": "/good-first-issue-finder/index.html"
+  },
+  {
+    "revision": "7cd35f55e9e173476755",
+    "url": "/good-first-issue-finder/static/css/main.d69cac64.chunk.css"
+  },
+  {
+    "revision": "bf91b3210a667b612b28",
+    "url": "/good-first-issue-finder/static/js/2.240c1dcc.chunk.js"
+  },
+  {
+    "revision": "7cd35f55e9e173476755",
+    "url": "/good-first-issue-finder/static/js/main.54f3c33b.chunk.js"
+  },
+  {
+    "revision": "6fa32a8183ca2e846645",
+    "url": "/good-first-issue-finder/static/js/runtime~main.62f42291.js"
+  }
+]);
