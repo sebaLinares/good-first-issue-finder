195a469e852d256354ba5572abc3a1eae0908559
Sebastian Linares <slinaresl@gmail.com>
2019-06-11 18:39:17 -0400

    Added Footer container

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// Import styles
+import styles from './Footer.module.scss'
+
+const Footer = props => (
+  <div className={styles.Footer}>
+    <p>
+      You can collaborate to this repo in{' '}
+      <a href="https://github.com/sebaLinares/good-first-issue-finder">
+        this github repository
+      </a>
+    </p>
+  </div>
+)
+
+export default Footer
